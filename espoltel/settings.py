3b43643bb0c1c2e110d81da6d72352b5b574503c
Sam Sánchez <samsvill@espol.edu.ec>
2024-08-17 21:54:38 -0500

    ya con la key ahí

--- espoltel/settings.py
+++ espoltel/settings.py
@@ -16,9 +16,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-2-=7=fg6&po!0266%95992wgu(ttorawr9%6-l_-)jsobju@ro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
